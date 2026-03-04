59fd94d9c2905564c8995b6c68a22d024bc18919
Julien-Wiegandt <wiegandtjulien2@gmail.com>
2026-03-04 18:14:11 +0100

    try deploy

--- apps/docs/.next/server/server-reference-manifest.js
+++ apps/docs/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iPfUYxIhGrx7iEz1QzDbH2uhe2Hy2jL8+LdDFNmf9zc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
